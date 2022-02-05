68a0c3a68d836a6e4bd7ddb8fc4e5e1bc0cbff9f
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-02-05 23:54:08 +0300

    implementations added

---
+++ app/src/main/java/com/asimodabas/country_detail/CountryFragment.kt
@@ -0,0 +1,26 @@
+package com.asimodabas.country_detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class CountryFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_country, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/com/asimodabas/country_detail/FeedFragment.kt
@@ -0,0 +1,23 @@
+package com.asimodabas.country_detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FeedFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_feed, container, false)
+    }
+}
