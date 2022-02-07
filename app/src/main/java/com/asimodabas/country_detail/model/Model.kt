8e7594dbd5baabe2449fbae610b77a8aeea0f66e
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-02-07 10:52:07 +0300

    country data class added

---
+++ app/src/main/java/com/asimodabas/country_detail/model/Model.kt
@@ -0,0 +1,10 @@
+package com.asimodabas.country_detail.model
+
+data class Country(
+    val countryName: String?,
+    val countryRegion: String?,
+    val countryLanguage: String?,
+    val imageUrl: String?,
+    val countryCurrency: String?,
+    val countryCapital: String?
+)
