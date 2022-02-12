ffa62a615c181f44dba2d866090a48b7a3afd51b
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-02-12 18:45:58 +0300

    country service and retrofit added

---
+++ app/src/main/java/com/asimodabas/country_detail/service/CountryServiceAPI.kt
@@ -0,0 +1,25 @@
+package com.asimodabas.country_detail.service
+
+import com.asimodabas.country_detail.model.Country
+import io.reactivex.Single
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+class CountryServiceAPI {
+
+    //https://raw.githubusercontent.com/asimodabas/Country-Detail/master/CountryData.json
+
+    private val BASE_URL = "https://raw.githubusercontent.com/"
+
+    private val api = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .build()
+        .create(CountryAPI::class.java)
+
+    fun getData ():Single<List<Country>>{
+        return api.getCountry()
+    }
+}
