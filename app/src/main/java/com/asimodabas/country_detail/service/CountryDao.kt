c2d31859f716a2414d4472bc769a946c6e16746c
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-02-16 19:27:45 +0300

    Dao added

---
+++ app/src/main/java/com/asimodabas/country_detail/service/CountryDao.kt
@@ -0,0 +1,20 @@
+package com.asimodabas.country_detail.service
+
+import androidx.room.Insert
+import androidx.room.Query
+import com.asimodabas.country_detail.model.Country
+
+interface CountryDao {
+
+    @Insert
+    suspend fun insertAll(vararg countries:Country) : List<Long>
+
+    @Query("SELECT * FROM country")
+    suspend fun getAllCountries():List<Country>
+
+    @Query("SELECT * FROM country WHERE uuid=:countryId")
+    suspend fun getCountry(countryId : Int) : Country
+
+    @Query("DELETE FROM country")
+    suspend fun deleteAllCountries()
+}
