207577be03954eb004dccdff95280171ee0eb1bb
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-02-17 19:02:56 +0300

    dao companion object+

--- app/src/main/java/com/asimodabas/country_detail/service/CountryDao.kt
+++ app/src/main/java/com/asimodabas/country_detail/service/CountryDao.kt
@@ -9,13 +9,13 @@ import com.asimodabas.country_detail.model.Country
 interface CountryDao {
 
     @Insert
-    suspend fun insertAll(vararg countries:Country) : List<Long>
+    suspend fun insertAll(vararg countries: Country): List<Long>
 
     @Query("SELECT * FROM country")
-    suspend fun getAllCountries():List<Country>
+    suspend fun getAllCountries(): List<Country>
 
     @Query("SELECT * FROM country WHERE uuid=:countryId")
-    suspend fun getCountry(countryId : Int) : Country
+    suspend fun getCountry(countryId: Int): Country
 
     @Query("DELETE FROM country")
     suspend fun deleteAllCountries()
--- app/src/main/java/com/asimodabas/country_detail/service/CountryDatabase.kt
+++ app/src/main/java/com/asimodabas/country_detail/service/CountryDatabase.kt
@@ -9,20 +9,25 @@ import com.asimodabas.country_detail.model.Country
 @Database(entities = arrayOf(Country::class), version = 1)
 abstract class CountryDatabase : RoomDatabase() {
 
-    abstract fun countryDao() : CountryDao
+    abstract fun countryDao(): CountryDao
 
-    companion object{
+    companion object {
 
-        @Volatile private var instance : CountryDatabase?=null
+        @Volatile
+        private var instance: CountryDatabase? = null
 
-        private val lock =Any()
+        private val lock = Any()
 
-        operator fun invoke(context: Context) = instance?: synchronized(lock){
-            instance?: makeDatabase(context).also {
-                instance=it
+        operator fun invoke(context: Context) = instance ?: synchronized(lock) {
+            instance ?: makeDatabase(context).also {
+                instance = it
             }
         }
 
-        private fun makeDatabase(context: Context)=Room.databaseBuilder(context.applicationContext, CountryDatabase::class.java, "countrydatabase").build()
+        private fun makeDatabase(context: Context) = Room.databaseBuilder(
+            context.applicationContext,
+            CountryDatabase::class.java,
+            "countrydatabase"
+        ).build()
     }
 }
