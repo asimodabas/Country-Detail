0a948c6feb7aa6b38895f58346530f823b742283
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-02-18 20:32:55 +0300

    Coroutine+

--- app/src/main/java/com/asimodabas/country_detail/viewmodel/BaseViewModel.kt
+++ app/src/main/java/com/asimodabas/country_detail/viewmodel/BaseViewModel.kt
@@ -7,11 +7,12 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlin.coroutines.CoroutineContext
 
-abstract class BaseViewModel(application: Application):AndroidViewModel(application),CoroutineScope {
+abstract class BaseViewModel(application: Application) : AndroidViewModel(application),
+    CoroutineScope {
     private val job = Job()
 
     override val coroutineContext: CoroutineContext
-        get() =job+Dispatchers.Main
+        get() = job + Dispatchers.Main
 
     override fun onCleared() {
         super.onCleared()
--- app/src/main/java/com/asimodabas/country_detail/viewmodel/FeedViewModel.kt
+++ app/src/main/java/com/asimodabas/country_detail/viewmodel/FeedViewModel.kt
@@ -1,15 +1,18 @@
 package com.asimodabas.country_detail.viewmodel
 
+import android.app.Application
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.asimodabas.country_detail.model.Country
+import com.asimodabas.country_detail.service.CountryDatabase
 import com.asimodabas.country_detail.service.CountryServiceAPI
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.launch
 
-class FeedViewModel : ViewModel() {
+class FeedViewModel(application: Application) : BaseViewModel(application) {
 
     private val countryApiService = CountryServiceAPI()
     private val disposable = CompositeDisposable()
@@ -24,7 +27,7 @@ class FeedViewModel : ViewModel() {
         getDataAPI()
     }
 
-    private fun getDataAPI(){
+    private fun getDataAPI() {
 
         countryLoading.value = true
 
@@ -32,7 +35,7 @@ class FeedViewModel : ViewModel() {
             countryApiService.getData()
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribeWith(object : DisposableSingleObserver<List<Country>>(){
+                .subscribeWith(object : DisposableSingleObserver<List<Country>>() {
                     override fun onSuccess(t: List<Country>) {
                         storeInSQLite(t)
                     }
@@ -47,13 +50,25 @@ class FeedViewModel : ViewModel() {
 
         )
     }
-    private fun showCountry(countryList : List<Country>){
+
+    private fun showCountry(countryList: List<Country>) {
         countries.value = countryList
         countryError.value = false
         countryLoading.value = false
     }
-    private fun storeInSQLite(list: List<Country>){
 
+    private fun storeInSQLite(list: List<Country>) {
+        launch {
+            val dao = CountryDatabase(getApplication()).countryDao()
+            dao.deleteAllCountries()
+            val listLong = dao.insertAll(*list.toTypedArray())
+            var i = 0
+            while (i<list.size){
+                list[i].uuid = listLong[i].toInt()
+                i = i+1
+            }
+            showCountry(list)
+        }
     }
 
 }
