8d246e804a51acf5b5e88adb40b884a3af1adcd5
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-02-19 20:57:11 +0300

    Shared preferences

---
+++ app/src/main/java/com/asimodabas/country_detail/util/CustomSharedPreferences.kt
@@ -0,0 +1,28 @@
+package com.asimodabas.country_detail.util
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.preference.PreferenceManager
+
+class CustomSharedPreferences {
+
+    companion object{
+
+        private var sharedPreferences : SharedPreferences?=null
+
+        @Volatile private var instance : CustomSharedPreferences?=null
+
+        private val lock  = Any()
+        operator fun invoke(context: Context) : CustomSharedPreferences= instance?: synchronized(lock){
+            instance?: makeCustomSharedPreferences(context).also {
+                instance = it
+            }
+        }
+        private fun makeCustomSharedPreferences(context: Context) : CustomSharedPreferences{
+            sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+            return CustomSharedPreferences()
+        }
+    }
+
+
+}
