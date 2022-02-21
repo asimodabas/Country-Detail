7f9f50ad5df271ae589600df79719a01180b36a2
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-02-21 15:27:06 +0300

    view binding added

--- app/src/main/java/com/asimodabas/country_detail/adapter/CountryAdapter.kt
+++ app/src/main/java/com/asimodabas/country_detail/adapter/CountryAdapter.kt
@@ -3,9 +3,11 @@ package com.asimodabas.country_detail.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.asimodabas.country_detail.R
+import com.asimodabas.country_detail.databinding.ItemCountryBinding
 import com.asimodabas.country_detail.model.Country
 import com.asimodabas.country_detail.util.downloadUrl
 import com.asimodabas.country_detail.util.placeHolderProgesBar
@@ -15,17 +17,22 @@ import kotlinx.android.synthetic.main.item_country.view.*
 class CountryAdapter(val countryList: ArrayList<Country>) :
     RecyclerView.Adapter<CountryAdapter.CountryViewHolder>() {
 
-    class CountryViewHolder(var view: View) : RecyclerView.ViewHolder(view) {
+    class CountryViewHolder(var view: ItemCountryBinding) : RecyclerView.ViewHolder(view.root) {
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {
         val infilater = LayoutInflater.from(parent.context)
-        val view = infilater.inflate(R.layout.item_country, parent, false)
+      //val view = infilater.inflate(R.layout.item_country, parent, false)
+        val view = DataBindingUtil.inflate<ItemCountryBinding>(infilater,R.layout.item_country,parent,false)
         return CountryViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
+
+        holder.view.country = countryList[position]
+
+        /*
 
         holder.view.name.text = countryList[position].countryName
         holder.view.region.text = countryList[position].countryRegion
@@ -37,8 +44,9 @@ class CountryAdapter(val countryList: ArrayList<Country>) :
         }
         holder.view.imageView.downloadUrl(
             countryList[position].imageUrl,
-            placeHolderProgesBar(holder.view.context)
-        )
+            placeHolderProgesBar(holder.view.context))
+
+        */
     }
 
     override fun getItemCount(): Int {
